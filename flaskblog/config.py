97e49e9e5815548f1f3c0791d05f090de6159897
Quan Nguyen Dinh <ndquan0612@gmail.com>
2019-05-02 11:20:23 +0700

    first commit

---
+++ flaskblog/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SECRET_KEY = 'fc49bfb02232e2311b1788a34ce4b3edc310ee59a6427d1b4814a33e3aced1a9'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
