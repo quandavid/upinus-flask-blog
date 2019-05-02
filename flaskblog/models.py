797d38ff99f5ac5780639ea995585587d6b932af
Quan Nguyen Dinh <ndquan0612@gmail.com>
2019-05-02 14:33:35 +0700

    fix implement postgres

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -9,6 +9,9 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
+
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -33,6 +36,9 @@ class User(db.Model, UserMixin):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 class Post(db.Model):
+
+    __tablename__ = 'posts'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
